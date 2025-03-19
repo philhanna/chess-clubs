c70a365a2d679ce5381461bca9cdb23b09a4c1aa
Phil Hanna <ph1204@gmail.com>
2025-03-19 17:40:20 -0400

    Added str method

--- src/games/game.py
+++ src/games/game.py
@@ -1,5 +1,7 @@
 class Game:
     def __init__(self):
+        self.player_id: str = None      # Player ID
+        self.player_name: str = None    # Player name
         self.tname: str = None          # Tournament name
         self.tid: str = None            # Tournament ID
         self.tdate: str = None          # Tournament date
@@ -9,3 +11,19 @@ class Game:
         self.opponent_id: str = None    # Opponent ID
         self.opponent_name: str = None  # Opponent name
         self.result: str = None         # Result
+        
+    def __str__(self) -> str:
+        parts = []
+        parts.append(f'player_id="{self.player_id}"')
+        parts.append(f'player_name="{self.player_name}"')
+        parts.append(f'tname="{self.tname}"')
+        parts.append(f'tid="{self.tid}"')
+        parts.append(f'tdate="{self.tdate}"')
+        parts.append(f'sname="{self.sname}"')
+        parts.append(f'rnumber="{self.rnumber}"')
+        parts.append(f'color="{self.color}"')
+        parts.append(f'opponent_id="{self.opponent_id}"')
+        parts.append(f'opponent_name="{self.opponent_name}"')
+        parts.append(f'result="{self.result}"')
+        output = ",".join(parts)
+        return output
