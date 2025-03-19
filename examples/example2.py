0fd6fe971befb8d115a154f1a9b5a968ef1bcdc5
Phil Hanna <ph1204@gmail.com>
2025-03-19 17:17:58 -0400

    Added example2

--- examples/example2.py
+++ examples/example2.py
@@ -1,20 +1,20 @@
 
-# Modify the system path before doing any imports of our specific code
 import os
 import sys
 
-
+# Modify the system path before doing any imports of our specific code
 this_file = os.path.abspath(__file__)
 examples_root = os.path.dirname(this_file)
 project_root = os.path.dirname(examples_root)
 src_dir = os.path.join(project_root, "src")
 sys.path.append(src_dir)
-sys.path.append(project_root)
+
+# Now do our imports
 from games.head_to_head import HeadToHead
 
 
 player_id = "12910923"
-opponent_id = "12877028"
+opponent_id = "13214962"
 obj = HeadToHead(player_id, opponent_id)
 for game in obj.games:
     print(game)
